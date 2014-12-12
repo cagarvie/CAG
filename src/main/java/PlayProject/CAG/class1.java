ae9c9e57b85d8f2ca5e49d09bfe0494616240858
Craig (Your Excellency) Garvie <craig.garvie@datadiscoveries.com>
2014-12-12 10:17:36 +0000

    update after merge

--- src/main/java/PlayProject/CAG/class1.java
+++ src/main/java/PlayProject/CAG/class1.java
@@ -7,5 +7,5 @@ public class class1 {
 	int b = 2; // Added a commennt 12/12 CAG
 	int c = 3; // Added Comment in Eclipse 12/12 CAG
 	int d = 4;// Added Comment in web checked in so conficly with  eclipse edit 12/12 CAG
-	
+
 }
