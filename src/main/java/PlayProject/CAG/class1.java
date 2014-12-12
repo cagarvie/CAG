1481442bb79c7cf35b0b642653b62460e2d82c8d
cagarvie <cagarvie@users.noreply.github.com>
2014-12-12 10:06:37 +0000

    Update class1.java

--- src/main/java/PlayProject/CAG/class1.java
+++ src/main/java/PlayProject/CAG/class1.java
@@ -6,5 +6,5 @@ public class class1 {
 	int a = 1;  // Added a comment 11/12 CAG
 	int b = 2; // Added a commennt 12/12 CAG
 	int c = 3;
-	int d = 4;
+	int d = 4;// Added Comment in web checked in so conficly with  eclipse edit 12/12 CAG
 }
