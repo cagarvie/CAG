1c67514a64328ba047b3531dd4e21340b1496a0c
Craig (Your Excellency) Garvie <craig.garvie@datadiscoveries.com>
2014-12-12 10:26:56 +0000

    merge after deleeted comments

--- src/main/java/PlayProject/CAG/class1.java
+++ src/main/java/PlayProject/CAG/class1.java
@@ -3,9 +3,11 @@ package PlayProject.CAG;
 public class class1 {
 
 	
-	int a = 1;  // Added a comment 11/12 CAG
+	int a = 1; 
 	int b = 2; // Added a commennt 12/12 CAG
 	int c = 3; // Added Comment in Eclipse 12/12 CAG
-	int d = 4;// Added Comment in web checked in so conficly with  eclipse edit 12/12 CAG
+	int d = 4;
 
 }
+
+
